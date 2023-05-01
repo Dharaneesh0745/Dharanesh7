33777a1a14feda956a2bee6826c8597ec075c4cd
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-05-01 21:35:28 +0530

    Very First unchanged files.

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-red-500 fixed top-0 w-[100%] z-10">
+      <div className="container mx-auto flex justify-between items-center py-4">
+        <div className="text-[24px">
+          Webloom
+        </div>
+        <div className="">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar

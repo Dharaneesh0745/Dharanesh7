aa08fd28676da2801b147dcb4913ea9e8d2a92a0
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-05-01 21:50:48 +0530

    Completed Navbar.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="bg-red-500 fixed top-0 w-[100%] z-10">
+    <div className="bg-black fixed top-0 w-[100%] z-10">
+
       <div className="container mx-auto flex justify-between items-center py-4">
-        <div className="text-[24px">
+
+        <div className="text-[24px] text-white ml-20">
           Webloom
         </div>
-        <div className="">
 
+        <div className="flex gap-6 text-white mr-20">
+          <Link href="/" className="hover:text-[orange]">
+            Home
+          </Link> 
+          <Link href="/courses" className="hover:text-[orange]">
+            Courses
+          </Link>
+          <Link href="/contact" className="hover:text-[orange]">
+            Contact Us
+          </Link>
         </div>
+
+
+
       </div>
     </div>
   )

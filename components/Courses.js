af973df0babcb3ea0f604853e63ad1dfc937864a
Dharaneesh0745 <123287037+Dharaneesh0745@users.noreply.github.com>
2023-05-01 23:18:26 +0530

    Updated Courses.js.
    Onclick Enroll button, it redirects to another webpage.

--- components/Courses.js
+++ components/Courses.js
@@ -12,15 +12,19 @@ function Courses() {
 
                 <h1 className="font-medium text-[30px] text-center">HTML Course</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="https://www.w3schools.com/html/default.asp">
                 <button className="bg-red-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">Enroll</button>
-            
+                </a>
+
             </div>
 
             <div className="shadow-lg flex flex-col gap-4 px-8 py-10 bg-[orange] text-white">
 
                 <h1 className="font-medium text-[30px] text-center">CSS Course</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="https://www.w3schools.com/css/default.asp">
                 <button className="bg-red-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">Enroll</button>
+                </a>
             
             </div>
 
@@ -28,8 +32,40 @@ function Courses() {
 
                 <h1 className="font-medium text-[30px] text-center">Java Script Course</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="https://www.w3schools.com/js/default.asp">
                 <button className="bg-red-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">Enroll</button>
-            
+                </a>
+
+            </div>
+
+            <div className="shadow-lg flex flex-col gap-4 px-8 py-10 bg-[orange] text-white">
+
+                <h1 className="font-medium text-[30px] text-center">Bootstrap Course</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="https://www.w3schools.com/bootstrap5/index.php">
+                <button className="bg-red-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">Enroll</button>
+                </a>
+
+            </div>
+
+            <div className="shadow-lg flex flex-col gap-4 px-8 py-10 bg-[orange] text-white">
+
+                <h1 className="font-medium text-[30px] text-center">React.js Course</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="https://www.w3schools.com/react/default.asp">
+                <button className="bg-red-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">Enroll</button>
+                </a>
+
+            </div>
+
+            <div className="shadow-lg flex flex-col gap-4 px-8 py-10 bg-[orange] text-white">
+
+                <h1 className="font-medium text-[30px] text-center">PHP Course</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a href="https://www.w3schools.com/php/default.asp">
+                <button className="bg-red-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">Enroll</button>
+                </a>
+
             </div>
 
         </div>
